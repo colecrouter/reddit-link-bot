514888b509d55a9cb8e3573bf1ea8772249ead7c
Cole Crouter <colecrouter1@gmail.com>
2023-11-12 12:13:06 -0600

    Added writeback for configs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,35 +11,41 @@ type Config struct {
 }
 
 func (c *Config) Load() error {
-	s, _ := os.Stat("./config.yml")
-	if s == nil {
-		f, err := os.Create("./config.yml")
-		if err != nil {
-			return err
-		}
-		defer f.Close()
+	// Read and write back the config file
+	// This is to ensure any new fields are added to the config file
 
-		def := &Config{
-			DiscordToken: "token",
-		}
-
-		b, err := yaml.Marshal(def)
-		if err != nil {
-			return err
-		}
-
-		_, err = f.Write(b)
-		if err != nil {
-			return err
-		}
+	f, err := os.OpenFile("./config.yml", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
 	}
 
+	defer f.Close()
+
+	// Create a default config
+	def := &Config{
+		DiscordToken: "token",
+	}
+
+	// Read the config file
 	b, err := os.ReadFile("./config.yml")
 	if err != nil {
 		return err
 	}
 
+	// Unmarshal the config file
 	err = yaml.Unmarshal(b, c)
+	if err != nil {
+		return err
+	}
+
+	// Marshal the default config
+	b, err = yaml.Marshal(def)
+	if err != nil {
+		return err
+	}
+
+	// Write the default config back to the file
+	_, err = f.Write(b)
 	if err != nil {
 		return err
 	}
