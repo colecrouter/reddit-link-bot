7d1394ddcfc8891d5c3934e76ebdd79162ac7a51
Cole Crouter <colecrouter1@gmail.com>
2023-12-16 14:49:33 -0600

    Add time delay for redirect handling in Scrape function

--- pkg/scrape/scrape.go
+++ pkg/scrape/scrape.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"path"
 	"regexp"
+	"time"
 
 	"github.com/Mexican-Man/reddit-bot/pkg/fetch"
 )
@@ -13,21 +14,33 @@ import (
 var regex = regexp.MustCompile(`(?:http|https):\/\/(?:www\.)?reddit\.com\/r\/([a-zA-Z0-9_]+)\/(comments|s)\/([a-zA-Z0-9_]+)?`)
 
 func Scrape(URL string) (media []Media, spoiler bool, nsfw bool, err error) {
-	groups := regex.FindAllStringSubmatch(URL, -1)
-	if len(groups[0]) == 0 {
-		err = fmt.Errorf("URL is not a reddit post")
-		return
-	}
+	var groups []string
 
-	// New Reddit app share links are weird. It redirects to a the proper URL, so we run a single Get,
-	// then it will follow all the redirects. Then, we get *that* URL and run the normal code.
-	if groups[0][2] == "s" {
+	for i := 0; ; i++ {
+		if i == 5 {
+			err = fmt.Errorf("unable to get to reddit post")
+			return
+		}
+
+		groups = regex.FindAllStringSubmatch(URL, -1)[0]
+		if len(groups) == 0 {
+			err = fmt.Errorf("URL is not a reddit post")
+			return
+		}
+
+		// New Reddit app share links are weird. It redirects to a the proper URL, so we run a single Get,
+		// then it will follow all the redirects. Then, we get *that* URL and run the normal code.
+		if groups[2] != "s" {
+			break
+		}
+
 		resp, _ := http.Get(URL)
-		return Scrape(resp.Request.URL.String())
+		URL = resp.Request.URL.String()
+		time.Sleep(1 * time.Second)
 	}
 
-	subreddit := groups[0][1]
-	id := groups[0][3]
+	subreddit := groups[1]
+	id := groups[3]
 
 	u, _ := url.Parse("https://www.reddit.com/")
 	u.Path = path.Join("r", subreddit, "comments", id, ".json")
